ccd71efa65474d95ed1bac9f9837ed2c709e8dfe
jack <784729370@qq.com>
2020-10-06 20:39:02 +0800

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.util.MyUtil;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+
+    Button btn1 = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("wan", "START");
+
+                MyUtil.start();
+
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/util/MyUtil.java
@@ -0,0 +1,13 @@
+package com.example.util;
+
+public class MyUtil {
+
+    //开始函数式编程
+    public static void start(){
+
+        //解析 APK
+
+
+    }
+
+}
